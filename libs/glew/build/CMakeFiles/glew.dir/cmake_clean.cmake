c2f1939cccebe6466a983c3517a2b7dad09b04ae
yilmaz <yilkan0510@gmail.com>
2024-12-15 17:42:05 +0100

    Setting up libraries

---
+++ libs/glew/build/CMakeFiles/glew.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/glew.dir/home/yilmaz/CLionProjects/Project-MB/libs/glew/src/glew.c.o"
+  "CMakeFiles/glew.dir/home/yilmaz/CLionProjects/Project-MB/libs/glew/src/glew.c.o.d"
+  "lib/libGLEW.pdb"
+  "lib/libGLEW.so"
+  "lib/libGLEW.so.2.1"
+  "lib/libGLEW.so.2.1.0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/glew.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
